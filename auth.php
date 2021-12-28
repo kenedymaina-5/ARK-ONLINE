3c8dedcdeae2a0f6a5e09c3d97ccf50e4949c9d3
kenedymaina-5 <76390492+kenedymaina-5@users.noreply.github.com>
2021-12-28 23:55:13 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbhost = "mysql-63940-0.cloudclusters.net:18440";
+$dbhost = "mysql-63949-0.cloudclusters.net:18440";
 $dbusername = "admin";
 $dbpassword = "5jWbCUPL";
 $dbname = "genesis2";
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
    $username = $_REQUEST["username"];
    $passwd = $_REQUEST["password_1"];
 }
-$smt = "SELECT * FROM genesis WHERE username='$username'";
+$smt = "SELECT * FROM genesis2 WHERE username='$username'";
 
 $result = $conn->query($smt);
 if ($result){
@@ -30,12 +30,12 @@ if ($result){
     if ($passwd == $conpasswd){
       header("Location: /HOME.html");
       }else{
-         header("Location: /index.html");
+         header("Location: /account.html");
       }
 
    }else{
       echo "failed";
-      header("location: /index.html");
+      header("location: /account.html");
    }
 
 
